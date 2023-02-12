0e4ef5b236072e554238670a34b8fe9adb606b42
Revathi-Kamineni <revathikamineni77@gmail.com>
2023-02-12 15:48:46 +0000

    palindrome

--- src/main/java/Palindrome.java
+++ src/main/java/Palindrome.java
@@ -12,6 +12,13 @@ public class Palindrome {
      * @return true if str is a palindrome, false otherwise.
      */
     public boolean pal(String str){
+        String pal="";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            pal = pal + str.charAt(i);
+        }
+        if (str.equals(pal)) {
+           return true;
+        }
         return false;
     }
 }
